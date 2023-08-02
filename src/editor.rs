aa936d456097cf17c24ecb9b9f873a2cb8f82d18
azur <natapat.samutpong@gmail.com>
2023-08-03 03:54:05 +0700

    Add paddings in UI

--- src/editor.rs
+++ src/editor.rs
@@ -40,94 +40,100 @@ pub(crate) fn create(
             // TODO: Implement waveform display
 
             ScrollView::new(cx, 0.0, 0.0, false, true, |cx| {
-                macro_rules! hstack {
-                    ($cx:ident, $f:expr) => {
-                        HStack::new($cx, $f)
-                        .child_top(Stretch(1.0))
-                        .child_bottom(Stretch(1.0))
-                        .col_between(Pixels(10.0))
-                        .left(Pixels(10.0))
-                    };
-                }
-                macro_rules! slider {
-                    ($cx:ident, $label:expr, $param:ident) => {
-                        hstack!($cx, |cx| {
-                            ParamSlider::new(cx, Data::params, |p| &p.$param);
-                            Label::new(cx, $label);
-                        })
-                    };
-                }
-                macro_rules! button {
-                    ($cx:ident, $label:expr, $param:ident) => {
-                        hstack!($cx, |cx| {
-                            ParamButton::new(cx, Data::params, |p| &p.$param);
-                            Label::new(cx, $label);
-                        })
-                    };
-                }
-                macro_rules! label {
-                    ($cx:ident, $label:expr) => {
-                        HStack::new($cx, |cx| {
-                            Label::new(cx, $label)
-                            .font_size(24.0);
-                        })
-                        .child_space(Stretch(1.0));
-                    };
-                }
+                VStack::new(cx, |cx| {
+                    macro_rules! hstack {
+                        ($cx:ident, $f:expr) => {
+                            HStack::new($cx, $f)
+                            .child_top(Stretch(1.0))
+                            .child_bottom(Stretch(1.0))
+                            .col_between(Pixels(10.0))
+                            .left(Pixels(10.0))
+                        };
+                    }
+                    macro_rules! slider {
+                        ($cx:ident, $label:expr, $param:ident) => {
+                            hstack!($cx, |cx| {
+                                ParamSlider::new(cx, Data::params, |p| &p.$param);
+                                Label::new(cx, $label);
+                            })
+                        };
+                    }
+                    macro_rules! button {
+                        ($cx:ident, $label:expr, $param:ident) => {
+                            hstack!($cx, |cx| {
+                                ParamButton::new(cx, Data::params, |p| &p.$param);
+                                Label::new(cx, $label);
+                            })
+                        };
+                    }
+                    macro_rules! label {
+                        ($cx:ident, $label:expr) => {
+                            HStack::new($cx, |cx| {
+                                Label::new(cx, $label)
+                                .font_size(24.0);
+                            })
+                            .child_space(Stretch(1.0));
+                        };
+                    }
 
-                label!(cx, "Mix");
-                slider!(cx, "Mix", mix);
-                button!(cx, "Hard Clip Output", output_clip);
-                slider!(cx, "Output Clip Threshold", output_clip_threshold);
+                    label!(cx, "Mix");
+                    slider!(cx, "Mix", mix);
+                    button!(cx, "Hard Clip Output", output_clip);
+                    slider!(cx, "Output Clip Threshold", output_clip_threshold);
 
-                label!(cx, "Waveshaper");
-                slider!(cx, "Pre Gain", pre_gain);
-                slider!(cx, "Function Mix", function_mix);
-                slider!(cx, "+ Function Type", pos_function_type);
-                slider!(cx, "+ Function Parameter", pos_function_param);
-                slider!(cx, "+ Function Mix", pos_function_mix);
-                slider!(cx, "- Function Type", neg_function_type);
-                slider!(cx, "- Function Parameter", neg_function_param);
-                slider!(cx, "- Function Mix", neg_function_mix);
-                slider!(cx, "Post Gain", post_gain);
-                slider!(cx, "Copy From", copy_function);
-                button!(cx, "Clip Function", clip_function);
-                button!(cx, "Flip Phase", flip);
+                    label!(cx, "Waveshaper");
+                    slider!(cx, "Pre Gain", pre_gain);
+                    slider!(cx, "Function Mix", function_mix);
+                    slider!(cx, "+ Function Type", pos_function_type);
+                    slider!(cx, "+ Function Parameter", pos_function_param);
+                    slider!(cx, "+ Function Mix", pos_function_mix);
+                    slider!(cx, "- Function Type", neg_function_type);
+                    slider!(cx, "- Function Parameter", neg_function_param);
+                    slider!(cx, "- Function Mix", neg_function_mix);
+                    slider!(cx, "Post Gain", post_gain);
+                    slider!(cx, "Copy From", copy_function);
+                    button!(cx, "Clip Function", clip_function);
+                    button!(cx, "Flip Phase", flip);
 
-                label!(cx, "Rectifier");
-                button!(cx, "Rectify", rectify);
-                slider!(cx, "Rectify Mix", rectify_mix);
-                slider!(cx, "Rectified Signal Mix In", rectify_mix_in);
-                slider!(cx, "Rectify Type", rectify_type);
-                button!(cx, "Flip Rectified Signal", rectify_flip);
+                    label!(cx, "Rectifier");
+                    button!(cx, "Rectify", rectify);
+                    slider!(cx, "Rectify Mix", rectify_mix);
+                    slider!(cx, "Rectified Signal Mix In", rectify_mix_in);
+                    slider!(cx, "Rectify Type", rectify_type);
+                    button!(cx, "Flip Rectified Signal", rectify_flip);
 
-                label!(cx, "Crusher");
-                button!(cx, "Crush", crush);
-                slider!(cx, "Crush Mix", crush_mix);
-                slider!(cx, "Crush Mix In", crush_mix_in);
-                slider!(cx, "Crush Type", crush_type);
-                slider!(cx, "Crush Step", crush_step);
+                    label!(cx, "Crusher");
+                    button!(cx, "Crush", crush);
+                    slider!(cx, "Crush Mix", crush_mix);
+                    slider!(cx, "Crush Mix In", crush_mix_in);
+                    slider!(cx, "Crush Type", crush_type);
+                    slider!(cx, "Crush Step", crush_step);
 
-                label!(cx, "Filter");
-                slider!(cx, "Excess Mix", excess_mix);
-                slider!(cx, "Low Pass", low_pass);
-                slider!(cx, "Low Pass Q", low_pass_q);
-                slider!(cx, "High Pass", high_pass);
-                slider!(cx, "High Pass Q", high_pass_q);
-                button!(cx, "Excess Signal Bypass", excess_bypass);
+                    label!(cx, "Filter");
+                    slider!(cx, "Excess Mix", excess_mix);
+                    slider!(cx, "Low Pass", low_pass);
+                    slider!(cx, "Low Pass Q", low_pass_q);
+                    slider!(cx, "High Pass", high_pass);
+                    slider!(cx, "High Pass Q", high_pass_q);
+                    button!(cx, "Excess Signal Bypass", excess_bypass);
+                })
+                .row_between(Pixels(10.0));
             })
             .width(Percentage(100.0));
 
-            Label::new(cx, &format!(
-                "{} by {} v{}",
-                crate::Penare::NAME,
-                crate::Penare::VENDOR,
-                crate::Penare::VERSION,
-            ))
+            HStack::new(cx, |cx| {
+                let small = 12.0;
+                Label::new(cx, crate::Penare::VENDOR)
+                .font_size(small);
+                Label::new(cx, crate::Penare::NAME)
+                .font_size(24.0);
+                Label::new(cx, crate::Penare::VERSION)
+                .font_size(small);
+            })
             .width(Percentage(100.0))
-            .height(Pixels(20.0))
-            .font_size(12.0)
+            .height(Pixels(40.0))
             .background_color(Color::rgb(200, 200, 200))
+            .child_space(Stretch(1.0))
             .child_top(Stretch(1.0))
             .child_bottom(Stretch(1.0));
         })
